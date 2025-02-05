d1e871ace7848079525c5e10ec8dd5cb93a7d1ad
Tim Fox <tim.mike.fox@gmail.com>
2025-02-04 21:19:57 -0500

    ListBuilding: lannister units
    Added the files I created before the repository was live. This is in a different structure than what was already published so we will sort out the similarities tomorrow.
    
    Main change introduces the /api/v1/units endpoint which currently just implements lannister combat unit data. The builder format is mainly a timesaver, longterm I suspect we will be saving the data in resources as JSON.

---
+++ src/main/java/asoiaf/Routes.java
@@ -0,0 +1,18 @@
+package asoiaf;
+
+import asoiaf.listbuilder.Units;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Routes {
+
+    @GetMapping("/units")
+    List<Units.Unit> allUnits() {
+        return Units.allUnits();
+    }
+}
