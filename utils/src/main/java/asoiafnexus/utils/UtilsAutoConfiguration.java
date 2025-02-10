395edcedb04653744355e91ed05d8c23b023d4d0
dmitriy-zhuk95 <114492498+dmitriy-zhuk95@users.noreply.github.com>
2025-02-10 09:55:09 +0100

    [Utils] Add utils model with springdoc-openapi-starter-webmvc dependency (#5)

---
+++ utils/src/main/java/asoiafnexus/utils/UtilsAutoConfiguration.java
@@ -0,0 +1,9 @@
+package asoiafnexus.utils;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@AutoConfiguration
+public class UtilsAutoConfiguration {
+}
